bf66461aaebeca25f0d8d946279608cbd6511a87
abhinavmabhi <dexmarketing192018@gmail.com>
2026-01-07 09:54:25 +0530

    test deployment

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://inspirezesttechnologiesprojectdemo.space/api/',
+        target: 'https://jajis.up.railway.app/api/',
         // target: 'http://localhost:8000/api/',
         changeOrigin: true,
         secure: true,
